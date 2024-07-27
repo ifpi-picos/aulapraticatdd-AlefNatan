9cef3252c31e398f52f54dd178b18d5060ee1d05
AlefNatan <104284234+AlefNatan@users.noreply.github.com>
2024-07-27 15:42:23 -0300

    Torna Dollar imutável e atualiza testes

--- Dollar.java
+++ Dollar.java
@@ -1,10 +1,9 @@
 public class Dollar {
-    int amount;
+    private int amount;
     Dollar(int amount) {
         this.amount = amount;
     }
-    void times(int multiplier) {
-        amount = amount * multiplier;
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
-
